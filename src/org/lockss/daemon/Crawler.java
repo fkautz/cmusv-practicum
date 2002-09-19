6fd510d7e090db6fa0ee438e3385314f6acfda7a
tal <tal>
2002-09-19 20:47:45 +0000

    org.lockss.daemon.DeadLine -> org.lockss.util.Deadline

--- src/org/lockss/daemon/Crawler.java
+++ src/org/lockss/daemon/Crawler.java
@@ -32,6 +32,7 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.daemon;
 import java.io.IOException;
+import org.lockss.util.Deadline;
 
 /**
  * This interface is implemented by the generic LOCKSS daemon.
@@ -57,5 +58,5 @@ public interface Crawler {
      * @exception java.io.IOException on any number of IO problems.
      */
     public CachedUrlSet fetch(CachedUrlSet target,
-			      DeadLine deadline) throws IOException;
+			      Deadline deadline) throws IOException;
 }
