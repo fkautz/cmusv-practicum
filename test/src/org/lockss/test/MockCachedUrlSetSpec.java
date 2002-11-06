ec6f65a7bcc1e76517b37fa29ea76bd9d5fcf109
troberts <troberts>
2002-11-06 01:40:33 +0000

    added setter for getPrefixList

--- test/src/org/lockss/test/MockCachedUrlSetSpec.java
+++ test/src/org/lockss/test/MockCachedUrlSetSpec.java
@@ -43,18 +43,29 @@ import org.lockss.util.*;
  * @version 0.0
  */
 
-public class MockCachedUrlSetSpec implements CachedUrlSetSpec{
+public class MockCachedUrlSetSpec implements CachedUrlSetSpec {
   private String root = null;
   private String regExp = null;
+  private List prefixList = null;
+
+
+  public MockCachedUrlSetSpec() {
+  }
 
   public MockCachedUrlSetSpec(String root, String regExp) {
     this.root = root;
     this.regExp = regExp;
+    this.prefixList =  ListUtil.list(root);
   }
   
   public List getPrefixList() {
-    return ListUtil.list(root);
+    return prefixList;
   }
+  
+  public void setPrefixList(List prefixList) {
+    this.prefixList = prefixList;
+  }
+
 
   public boolean matches(String url) {
     return url.startsWith(root);
