07401321ab7378836c7b995ed92259f364f48ff0
aalto <aalto>
2002-11-13 05:09:49 +0000

    Added some functionality to MockMessageDigest, checked in functional test for the simulated content.  Currently has plugin registration, content generation, content crawling, cache checking, and hash checking.

--- test/src/org/lockss/test/MockMessageDigest.java
+++ test/src/org/lockss/test/MockMessageDigest.java
@@ -122,17 +122,28 @@ public class MockMessageDigest extends MessageDigest {
     return length;
   }
 
-  protected void engineUpdate(byte input){
+  protected void engineUpdate(byte input) {
+    update(input);
   }
 
-  protected void engineReset(){
+  protected void engineReset() {
+    inputList = new Vector();
   }
 
-  protected void engineUpdate(byte[] input, int offset, int len){
+  protected void engineUpdate(byte[] input, int offset, int len) {
+    if ((len>0) && (input.length>=offset+len)) {
+      for (int ii=0; ii<len; ii++) {
+        engineUpdate(input[offset+ii]);
+      }
+    }
   }
 
-  protected byte[] engineDigest(){
-    return null;
+  protected byte[] engineDigest() {
+    byte[] bytes = new byte[inputList.size()];
+    for (int ii=0; ii<inputList.size(); ii++ ) {
+      bytes[ii] = ((Byte)inputList.elementAt(ii)).byteValue();
+    }
+    return bytes;
   }
 
 
