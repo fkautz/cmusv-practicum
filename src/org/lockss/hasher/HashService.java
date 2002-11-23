c4ad9222951283f72ba1a38a793153c8cb58b0b9
tal <tal>
2002-11-23 01:51:47 +0000

    Fixed bugs from code review.

--- src/org/lockss/hasher/HashService.java
+++ src/org/lockss/hasher/HashService.java
@@ -115,7 +115,8 @@ public class HashService {
     HashQueue.Request req =
       new HashQueue.Request(urlset, hasher, deadline,
 			    callback, cookie,
-			    urlset.getNameHasher(hasher), 0); 
+			    // tk - get better duration estimate
+			    urlset.getNameHasher(hasher), 1000); 
     return scheduleReq(req);
   }
 
