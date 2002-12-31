d7ec7d3734b4990cacf1859a5c47fdd45f024a9a
aalto <aalto>
2002-12-31 22:44:34 +0000

    Used defaults properly.

--- test/src/org/lockss/daemon/TestSystemMetrics.java
+++ test/src/org/lockss/daemon/TestSystemMetrics.java
@@ -49,8 +49,8 @@ public class TestSystemMetrics extends LockssTestCase {
   public void setUp() throws Exception {
     super.setUp();
     metrics = new SystemMetrics();
-    configHashParams(SystemMetrics.DEFAULT_HASH_DURATION,
-                     SystemMetrics.DEFAULT_HASH_STEP);
+    configHashParams(Integer.parseInt(SystemMetrics.DEFAULT_HASH_DURATION),
+                     Integer.parseInt(SystemMetrics.DEFAULT_HASH_STEP));
   }
 
   public void testHashEstimation() throws IOException {
@@ -61,7 +61,8 @@ public class TestSystemMetrics extends LockssTestCase {
     long endTime = TimeBase.nowMs();
     assertTrue(estimate > 0);
     //XXX fix using simulated time
-    assertTrue(endTime - startTime > (100000/SystemMetrics.DEFAULT_HASH_STEP));
+    int expectedTime = (10000 * 10) / Integer.parseInt(SystemMetrics.DEFAULT_HASH_STEP);
+    assertTrue(endTime - startTime > expectedTime);
   }
 
   public void testEstimationCaching() throws IOException {
