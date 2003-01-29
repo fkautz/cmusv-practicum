ed8f6f7cb6c30f64e84285fe772beffec9f12664
claire <claire>
2003-01-29 20:19:51 +0000

    Fix failure caused by testing .canWrite on a file that has not been created.

--- src/org/lockss/protocol/IdentityManager.java
+++ src/org/lockss/protocol/IdentityManager.java
@@ -246,7 +246,8 @@ public class IdentityManager {
         iddbDir.mkdirs();
       }
       File iddbFile = new File(iddbDir, IDDB_FILENAME);
-      if((iddbFile != null) && iddbFile.canWrite()) {
+
+      if((iddbFile != null) && iddbFile.createNewFile() && iddbFile.canWrite()) {
         IdentityListBean idlb = getIdentityListBean();
         Marshaller marshaller = new Marshaller(new FileWriter(iddbFile));
         marshaller.setMapping(getMapping());
@@ -336,7 +337,12 @@ public class IdentityManager {
 
     if (mapping==null) {
       String mappingFile = Configuration.getParam(PARAM_IDDB_MAP_DIR)
-                           + File.separator + IDDB_MAP_FILENAME;
+                         + File.separator + IDDB_MAP_FILENAME;
+      File mpFile =  mpFile = new File(mappingFile);
+      if(mpFile == null || !mpFile.exists()) {
+        theLog.error("Unable to find mapping file: " + mpFile.getName());
+        return null;
+      }
 
       Mapping map = new Mapping();
       try {
