c0248059b727efd3551d95e6b196cb44714ddafc
troberts <troberts>
2003-03-13 18:54:37 +0000

    org.lockss.daemon.status.StatusServiceImpl *extends BaseLockssManager to act as a LockssManager

--- src/org/lockss/daemon/status/StatusServiceImpl.java
+++ src/org/lockss/daemon/status/StatusServiceImpl.java
@@ -33,11 +33,13 @@ in this Software without prior written authorization from Stanford University.
 package org.lockss.daemon.status;
 
 import java.util.*;
+import org.lockss.app.*;
 
 /**
  * Main implementation of {@link StatusService}
  */
-public class StatusServiceImpl implements StatusService {
+public class StatusServiceImpl 
+  extends BaseLockssManager implements StatusService {
   private Map statusAccessors;
 
 
