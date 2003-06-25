cf8c2f54dd62fce4fac2a7bfeb9d0639c78ff0e9
troberts <troberts>
2003-06-25 23:40:19 +0000

    org.lockss.CrawlManagerImpl *changed ListUtil.listToString to StringUtil.separatedString

--- src/org/lockss/crawler/CrawlManagerImpl.java
+++ src/org/lockss/crawler/CrawlManagerImpl.java
@@ -466,7 +466,7 @@ public class CrawlManagerImpl extends BaseLockssManager
       row.put(NUM_URLS_FETCHED, new Long(crawler.getNumFetched()));
       row.put(NUM_URLS_PARSED, new Long(crawler.getNumParsed()));
       row.put(START_URLS,
-	      (ListUtil.listToString(crawler.getStartUrls(), "\n")));
+	      (StringUtil.separatedString(crawler.getStartUrls(), "\n")));
       if (au instanceof BaseArchivalUnit) {
 	BaseArchivalUnit bau = (BaseArchivalUnit)au;
 	row.put(NUM_CACHE_HITS, new Long(bau.getCrawlSpecCacheHits()));
