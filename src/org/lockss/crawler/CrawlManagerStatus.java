94608c3ee90f68d3bf27d0c0e88ad02ed27b309f
troberts <troberts>
2003-07-18 22:18:24 +0000

    *now correctly displays crawl type

--- src/org/lockss/crawler/CrawlManagerStatus.java
+++ src/org/lockss/crawler/CrawlManagerStatus.java
@@ -145,7 +145,13 @@ public class CrawlManagerStatus implements StatusAccessor {
   }
 
   private String getTypeString(int type) {
-    return "blah";
+    switch(type) {
+      case Crawler.NEW_CONTENT:
+	return NC_TYPE;
+      case Crawler.REPAIR:
+	return REPAIR_TYPE;
+    }
+    return "Unknown";
   }
 
   private Map makeRow(Crawler crawler) {
