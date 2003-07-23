ac282f4952e19141e7e9fcc2dfaadacfaafefaf9
tlipkis <tlipkis>
2003-07-23 06:40:57 +0000

    Log form params at debug().

--- src/org/lockss/servlet/LockssServlet.java
+++ src/org/lockss/servlet/LockssServlet.java
@@ -697,7 +697,7 @@ public abstract class LockssServlet extends HttpServlet
     return theDaemon;
   }
 
-  private void logParams() {
+  protected void logParams() {
     Enumeration en = req.getParameterNames();
     while (en.hasMoreElements()) {
       String name = (String)en.nextElement();
@@ -714,7 +714,7 @@ public abstract class LockssServlet extends HttpServlet
     try {
       this.req = req;
       this.resp = resp;
-      if (log.isDebug3()) {
+      if (log.isDebug()) {
 	logParams();
       }
       resp.setContentType("text/html");
