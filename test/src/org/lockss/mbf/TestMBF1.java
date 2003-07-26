543777d54957e8d305cf9a00f0fc008e82f5dac3
dshr <dshr>
2003-07-26 01:19:30 +0000

    Only do expensive table generation once for all MBF tests.

--- test/src/org/lockss/mbf/TestMBF1.java
+++ test/src/org/lockss/mbf/TestMBF1.java
@@ -53,15 +53,16 @@ public class TestMBF1 extends LockssTestCase {
       rand = new Random(100);
     else 
       rand = new Random(System.currentTimeMillis());
-    f = FileUtil.tempFile("mbf1test");
-    FileOutputStream fis = new FileOutputStream(f);
-    basis = new byte[16*1024*1024 + 1024];
-    rand.nextBytes(basis);
-    fis.write(basis);
-    fis.close();
-    if (f.exists()) {
-      log.debug(f.getPath() + " bytes " + f.length() + " long");
-    } else
+    if (f == null) {
+      f = FileUtil.tempFile("mbf1test");
+      FileOutputStream fis = new FileOutputStream(f);
+      basis = new byte[16*1024*1024 + 1024];
+      rand.nextBytes(basis);
+      fis.write(basis);
+      fis.close();
+      log.info(f.getPath() + " bytes " + f.length() + " long created");
+    }
+    if (!f.exists())
       log.warning(f.getPath() + " doesn't exist");
     MemoryBoundFunction.configure(f);
   }
