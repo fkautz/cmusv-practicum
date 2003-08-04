5b1631875b97022b33fbc3ad5524c994256f43cf
tlipkis <tlipkis>
2003-08-04 07:57:22 +0000

    Removed SimulatedPlugin from registry, so it doesn't show up in menus (unless loaded anyway because it's configured).

--- src/org/lockss/plugin/PluginManager.java
+++ src/org/lockss/plugin/PluginManager.java
@@ -491,9 +491,12 @@ public class PluginManager extends BaseLockssManager {
   }
 
   // Plugin registry
+  // List the plugins that should always be listed in UI menus.
+  // Other plugins that were loaded because of an configured AU will
+  // be included automatically
   private String builtinPluginNames[] = {
     "org.lockss.plugin.highwire.HighWirePlugin",
-    "org.lockss.plugin.simulated.SimulatedPlugin",
+//     "org.lockss.plugin.simulated.SimulatedPlugin",
   };
 
   public Collection getRegisteredPlugins() {
