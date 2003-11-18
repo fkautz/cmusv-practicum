ae38e9613b20905d1fac369f7407ae11106853a5
eaalto <eaalto>
2003-11-18 00:07:16 +0000

    Added History Cooperative plugin.

--- src/org/lockss/plugin/PluginManager.java
+++ src/org/lockss/plugin/PluginManager.java
@@ -620,7 +620,8 @@ public class PluginManager extends BaseLockssManager {
     "org.lockss.plugin.acs.AcsPlugin",
     "org.lockss.plugin.absinthe.AbsinthePlugin",
     "org.lockss.plugin.emls.EmlsPlugin",
-    "org.lockss.plugin.ieee.IeeePlugin"
+    "org.lockss.plugin.ieee.IeeePlugin",
+    "org.lockss.plugin.histcoop.HistoryCooperativePlugin"
 //     "org.lockss.plugin.simulated.SimulatedPlugin",
   };
 
