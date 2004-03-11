26b4c5c06149410dbc4ac380292c7a102ed2fe74
eaalto <eaalto>
2004-03-11 02:29:31 +0000

    Made 'getAuManager()' static to match 'getManager()'.

--- src/org/lockss/app/LockssDaemon.java
+++ src/org/lockss/app/LockssDaemon.java
@@ -470,7 +470,7 @@ private final static String LOCKSS_USER_AGENT = "LOCKSS cache";
    * @return a LockssAuManager
    * @throws IllegalArgumentException if the manager is not available.
    */
-  public LockssAuManager getAuManager(String key, ArchivalUnit au) {
+  public static LockssAuManager getAuManager(String key, ArchivalUnit au) {
     LockssAuManager mgr = null;
     SequencedHashMap auMgrMap =
       (SequencedHashMap)auManagerMaps.get(au);
