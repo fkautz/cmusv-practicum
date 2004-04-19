b234d45a796eb02b375369e522c84939f10102ec
tlipkis <tlipkis>
2004-04-19 19:02:23 +0000

    Added setReader()

--- test/src/org/lockss/test/MockCachedUrl.java
+++ test/src/org/lockss/test/MockCachedUrl.java
@@ -54,6 +54,7 @@ public class MockCachedUrl implements CachedUrl {
 
   private boolean doesExist = false;
   private String content = null;
+  private Reader reader = null;
 
   public MockCachedUrl(String url) {
     this.url = url;
@@ -80,6 +81,10 @@ public class MockCachedUrl implements CachedUrl {
     if (content != null) {
       return new StringReader(content);
     }
+    if (reader != null) {
+      return reader;
+    }
+
     throw new UnsupportedOperationException("Not implemented");
   }
 
@@ -147,6 +152,10 @@ public class MockCachedUrl implements CachedUrl {
 
   public void setContent(String content) {
     this.content = content;
+  }
+
+  public void setReader(Reader reader) {
+    this.reader = reader;
   }
 
   public void setProperties(CIProperties prop){
