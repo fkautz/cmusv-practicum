09b3ba9a2a4d02067a4a4b45c25c0690ea382f56
tlipkis <tlipkis>
2004-04-29 10:11:17 +0000

    Added getTable() with options.

--- src/org/lockss/daemon/status/StatusServiceImpl.java
+++ src/org/lockss/daemon/status/StatusServiceImpl.java
@@ -58,6 +58,11 @@ public class StatusServiceImpl
 
   public StatusTable getTable(String tableName, String key) 
       throws StatusService.NoSuchTableException {
+    return getTable(tableName, key, null);
+  }
+
+  public StatusTable getTable(String tableName, String key, BitSet options) 
+      throws StatusService.NoSuchTableException {
     if (tableName == null) {
       throw new 
 	StatusService.NoSuchTableException("Called with null tableName");
@@ -73,6 +78,11 @@ public class StatusServiceImpl
 						   +tableName+" "+key);
     } 
     StatusTable table = new StatusTable(tableName, key);
+    if (options != null) {
+      BitSet tableOpts = table.getOptions();
+      tableOpts.xor(tableOpts);
+      tableOpts.or(options);
+    }
     statusAccessor.populateTable(table);
     if (table.getTitle() == null) {
       try {
