2a83dcdc072547594d76a4f4c6e200879fb57620
tlipkis <tlipkis>
2004-06-17 00:03:43 +0000

    Made runAllExpired0() not loop.

--- src/org/lockss/util/TimerQueue.java
+++ src/org/lockss/util/TimerQueue.java
@@ -97,27 +97,27 @@ public class TimerQueue /*extends BaseLockssManager*/ implements Serializable {
   }
 
   private void runAllExpired0() {
-    while (true) {
-      Request req = (Request)queue.peek();
-      if (req == null || !req.deadline.expired()) {
-	return;
-      }
-      // Need to wait until expired requests run.  Easiest way is to put
-      // our own request on the queue (which will come after any with earlier
-      // or equal deadlines) and wait for it to happen.
-      final BinarySemaphore sem = new BinarySemaphore();
-      add(Deadline.in(0),
-	  new Callback() {
-	    public void timerExpired(Object cookie) {
-	      sem.give();
-	    }},
-	  null);
-      try {
-	sem.take(Deadline.in(Constants.SECOND));
-      } catch (InterruptedException e) {
-      }
+    Request req = (Request)queue.peek();
+    if (req == null || !req.deadline.expired()) {
+      // queue is empty, or there are no unexpired requests
+      return;
+    }
+    // Need to wait until expired requests run.  Easiest way is to put
+    // our own request on the queue (which will come after any with earlier
+    // or equal deadlines) and wait for it to happen.
+    final BinarySemaphore sem = new BinarySemaphore();
+    add(Deadline.in(0),
+	new Callback() {
+	  public void timerExpired(Object cookie) {
+	    sem.give();
+	  }},
+	null);
+    try {
+      sem.take(Deadline.MAX);
+    } catch (InterruptedException e) {
     }
   }
+
 
   /** Timer Request element; only used to cancel a request. */
   public class Request implements Serializable, Comparable {
