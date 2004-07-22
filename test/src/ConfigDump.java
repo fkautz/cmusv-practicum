29d7c6c669155ecdfb8270a40826d4beff7c04a4
tlipkis <tlipkis>
2004-07-22 20:20:24 +0000

    Fixed class comment.

--- test/src/ConfigDump.java
+++ test/src/ConfigDump.java
@@ -38,8 +38,8 @@ import org.lockss.plugin.*;
 import org.lockss.test.*;
 
 /**
- * Utility to create title db property files.  Queries user for plugin and
- * parameter values, or dumps all entries known by known plugins.
+ * Utility to load and dump config files in a canonical form, for easy
+ * diffing.
  */
 public class ConfigDump {
 
