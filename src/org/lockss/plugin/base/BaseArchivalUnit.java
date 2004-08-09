316949613f65391127ec37eb10820b03b53eb99d
tlipkis <tlipkis>
2004-08-09 19:19:49 +0000

    Use ISO-8859-1 for permission filter reader.

--- src/org/lockss/plugin/base/BaseArchivalUnit.java
+++ src/org/lockss/plugin/base/BaseArchivalUnit.java
@@ -431,9 +431,8 @@ public abstract class BaseArchivalUnit implements ArchivalUnit {
     String matchstr = PERMISSION_STRING.toLowerCase();
     boolean wasWhiteSpace = false;  // last char was ws
 
-    Reader filteredReader = getCrawlPermissionFilter(reader);
-
     try {
+      Reader filteredReader = getCrawlPermissionFilter(reader);
       do {
         ch = filteredReader.read();
         char nextChar = matchstr.charAt(p_index);
@@ -454,7 +453,7 @@ public abstract class BaseArchivalUnit implements ArchivalUnit {
     return crawl_ok;
   }
 
-  private Reader getCrawlPermissionFilter(Reader reader) {
+  private Reader getCrawlPermissionFilter(Reader reader) throws IOException {
     // convert html tags to whitespace
     Reader replaceReader = StringFilter.makeNestedFilter(reader,
         new String[][] {
@@ -466,7 +465,7 @@ public abstract class BaseArchivalUnit implements ArchivalUnit {
     // condense whitespace
     InputStream wsIs =
         new WhiteSpaceFilter(new ReaderInputStream(replaceReader));
-    return new InputStreamReader(wsIs);
+    return new InputStreamReader(wsIs, Constants.DEFAULT_ENCODING);
   }
 
   /**
