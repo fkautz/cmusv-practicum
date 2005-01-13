c35bbf51b650f38953e3cf5112ad11ef16854a3b
dcfok <dcfok>
2005-01-13 00:51:11 +0000

    javadoc added

--- src/org/lockss/oai/Oai_dcHandler.java
+++ src/org/lockss/oai/Oai_dcHandler.java
@@ -34,14 +34,21 @@ package org.lockss.oai;
 
 import org.lockss.util.Logger;
 
+/**
+ * Oai_dcHandler extends BaseOaiMetadataHandler to handler oai response with
+ * oai_dc metadata format.
+ */
 public class Oai_dcHandler extends BaseOaiMetadataHandler {
-    protected static Logger logger = Logger.getLogger("Oai_dcHandler");
+  protected static Logger logger = Logger.getLogger("Oai_dcHandler");
 
-    public Oai_dcHandler()
-    {
-        metadataPrefix = "oai_dc";
-        metadataNamespaceUrl = "http://purl.org/dc/elements/1.1/";
-        urlContainerTagName = "identifier";
-    }
+  /**
+   * Constructor set all the necessary information to handle metadata record
+   * in oai_dc format properly.
+   */
+  public Oai_dcHandler()    {
+    metadataPrefix = "oai_dc";
+    metadataNamespaceUrl = "http://purl.org/dc/elements/1.1/";
+    urlContainerTagName = "identifier";
+  }
 
 }
