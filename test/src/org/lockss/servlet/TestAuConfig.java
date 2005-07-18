4c13363c634eed81b2e09f36438b363a6aadb391
tlipkis <tlipkis>
2005-07-18 08:07:47 +0000

    Rewrote/simplified agree map handling and API. Added writeIdentityAgreementTo(), readIdentityAgreementFrom() for backing  up/restoring agree maps.

--- test/src/org/lockss/servlet/TestAuConfig.java
+++ test/src/org/lockss/servlet/TestAuConfig.java
@@ -57,6 +57,7 @@ public class TestAuConfig extends LockssServletTestCase {
     super.setUp();
     pluginMgr = new PluginManager();
     theDaemon.setPluginManager(pluginMgr);
+    getMockLockssDaemon().setIdentityManager(new org.lockss.protocol.MockIdentityManager());
     theDaemon.setDaemonInited(true);
     theDaemon.getRemoteApi().startService();
     pluginMgr.initService(theDaemon);
