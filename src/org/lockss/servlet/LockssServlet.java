09216898a162e287585acc08cb2abd314fc90b50
thib_gc <thib_gc>
2005-10-14 21:24:13 +0000

    Fixed IP allow/deny glitch. Factored out error messages and status messages.

--- src/org/lockss/servlet/LockssServlet.java
+++ src/org/lockss/servlet/LockssServlet.java
@@ -674,6 +674,10 @@ public abstract class LockssServlet extends HttpServlet
 
   private boolean isFramed = false;
 
+  protected String errMsg;
+
+  protected String statusMsg;
+
   protected boolean isFramed() {
     return isFramed;
   }
@@ -999,6 +1003,12 @@ public abstract class LockssServlet extends HttpServlet
   /** Convenience method */
   protected String encodeAttr(String s) {
     return HtmlUtil.encode(s, HtmlUtil.ENCODE_ATTR);
+  }
+
+  /** Create message and error message block 
+   * @param composite TODO*/
+  protected void layoutErrorBlock(Composite composite) {
+    ServletUtil.layoutErrorBlock(composite, errMsg, statusMsg);
   }
 
   /** Exception thrown if multipart form data is longer than the
