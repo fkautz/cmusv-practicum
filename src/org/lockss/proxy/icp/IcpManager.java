8fd10107ea966ce78e0e6348fcdf9b9530c3a52b
thib_gc <thib_gc>
2005-11-07 19:57:20 +0000

    Fixed startup bug in the ICP manager.

--- src/org/lockss/proxy/icp/IcpManager.java
+++ src/org/lockss/proxy/icp/IcpManager.java
@@ -202,7 +202,7 @@ public class IcpManager
       boolean enable = newConfig.getBoolean(PARAM_ICP_ENABLED,
                                             DEFAULT_ICP_ENABLED);
       stopSocket();
-      if (enable) {
+      if (isDaemonInited() && enable) {
         startSocket();
       }
     }
