7ebb9a0420581743c04c264f8d4cdcfc6b627e12
thib_gc <thib_gc>
2006-01-12 01:12:50 +0000

    Cosmetic.

--- src/org/lockss/servlet/ThreadDump.java
+++ src/org/lockss/servlet/ThreadDump.java
@@ -1,10 +1,10 @@
-// ========================================================================
-// $Id$
-// ========================================================================
+/*
+ * $Id$
+ */
 
 /*
 
-Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
