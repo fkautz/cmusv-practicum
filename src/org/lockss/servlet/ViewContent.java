d7a924445723bb2e519d5c1081a48e0ccbe25f7d
tlipkis <tlipkis>
2006-06-01 23:48:21 +0000

    Access version number via CU instead of repository.

--- src/org/lockss/servlet/ViewContent.java
+++ src/org/lockss/servlet/ViewContent.java
@@ -47,7 +47,6 @@ import org.lockss.daemon.*;
 import org.lockss.jetty.*;
 import org.lockss.plugin.*;
 import org.lockss.state.*;
-import org.lockss.repository.*;
 
 /** ViewContent servlet displays cached content
  */
@@ -167,7 +166,6 @@ public class ViewContent extends LockssServlet {
   }
 
   void displaySummary(boolean contentInOtherFrame) throws IOException {
-    LockssRepository repo = getLockssDaemon().getLockssRepository(au);
     Page page = newPage();
 
     Table tbl = new Table(0, "ALIGN=CENTER CELLSPACING=2 CELLPADDING=0");
@@ -191,10 +189,9 @@ public class ViewContent extends LockssServlet {
 	       props.getProperty(CachedUrl.PROPERTY_CONTENT_TYPE));
     addPropRow(tbl, "Length", clen);
     try {
-      RepositoryNode node = repo.getNode(cu.getUrl());
-      addPropRow(tbl, "Version #", node.getCurrentVersion());
-    } catch (MalformedURLException e) {
-      log.warning("Can't get repo node: " + cu.getUrl(), e);
+      addPropRow(tbl, "Version #", cu.getVersion());
+    } catch (RuntimeException e) {
+      log.warning("Can't get cu version: " + cu.getUrl(), e);
     }
     try {
       long sdate =
