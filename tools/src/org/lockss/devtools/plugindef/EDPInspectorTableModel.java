841215e038a19e3e21f98d1654faa303275626bc
thib_gc <thib_gc>
2006-06-26 17:46:56 +0000

    Missing license and copyright statements. Copyright year updates.

--- tools/src/org/lockss/devtools/plugindef/EDPInspectorTableModel.java
+++ tools/src/org/lockss/devtools/plugindef/EDPInspectorTableModel.java
@@ -3,7 +3,7 @@
  */
 
 /*
-  Copyright (c) 2000-2004 Board of Trustees of Leland Stanford Jr. University,
+  Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
   all rights reserved.
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
