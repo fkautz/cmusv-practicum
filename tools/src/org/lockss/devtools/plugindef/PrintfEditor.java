7627f8ca1d17db3558685edaf993da76aeedf1a3
thib_gc <thib_gc>
2006-07-10 16:54:46 +0000

    Issue 1939: fixed a bug that caused the plugin tool to mis-handle percent signs in printf specifications.

--- tools/src/org/lockss/devtools/plugindef/PrintfEditor.java
+++ tools/src/org/lockss/devtools/plugindef/PrintfEditor.java
@@ -552,7 +552,7 @@ public class PrintfEditor extends JDialog implements EDPEditor, ConfigParamListe
       java.util.List elements = editableTemplate.getPrintfElements();
       for(Iterator it = elements.iterator(); it.hasNext(); ) {
         PrintfUtil.PrintfElement el = (PrintfUtil.PrintfElement) it.next();
-        if(el.getFormat().equals("\0")) {
+        if(el.getFormat().equals(PrintfUtil.PrintfElement.NONE)) {
           appendText(el.getElement(), PLAIN_ATTR);
         }
         else {
@@ -563,9 +563,9 @@ public class PrintfEditor extends JDialog implements EDPEditor, ConfigParamListe
       }
     }
     catch(Exception ex) {
-      JOptionPane.showMessageDialog(this,"Invalid Format:" + ex.getMessage(),
+      JOptionPane.showMessageDialog(this, "Invalid Format: " + ex.getMessage(),
                                     "Invalid Printf Format",
-                                    JOptionPane.WARNING_MESSAGE);
+                                    JOptionPane.ERROR_MESSAGE);
 
       selectedPane = 1;
       printfTabPane.setSelectedIndex(selectedPane);
