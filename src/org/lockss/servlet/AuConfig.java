b502e9ea8147e09f93d934c5ae633c360a9b34cd
tlipkis <tlipkis>
2006-07-17 07:13:24 +0000

    Don't offer "down" titles (issue 1927)

--- src/org/lockss/servlet/AuConfig.java
+++ src/org/lockss/servlet/AuConfig.java
@@ -383,10 +383,16 @@ public class AuConfig extends LockssServlet {
       sel.add("-no selection-", true, "");
       for (Iterator iter = titles.iterator(); iter.hasNext(); ) {
 	String title = (String)iter.next();
+	PluginProxy titlePlugin = getTitlePlugin(title);
+	if (titlePlugin != null) {
+	  TitleConfig tc = titlePlugin.getTitleConfig(title);
+	  if (tc != null && AuUtil.isPubDown(tc)) {
+	    continue;
+	  }
+	}
 	String selText = encodeText(title);
 	String dispText = selText;
 	if (includePluginInTitleSelect) {
-	  PluginProxy titlePlugin = getTitlePlugin(title);
 	  if (titlePlugin != null) {
 	    String plugName = titlePlugin.getPluginName();
 	    dispText = selText + " (" + plugName + ")";
