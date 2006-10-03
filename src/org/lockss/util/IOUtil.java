6b31c8f21e9f256be190005001a69583d32c5d9b
smorabito <smorabito>
2006-10-03 21:36:09 +0000

    Fix for file descriptor leak in DiskVoteBlocks

--- src/org/lockss/util/IOUtil.java
+++ src/org/lockss/util/IOUtil.java
@@ -105,7 +105,13 @@ public class IOUtil {
       s.close();
     } catch (Exception e) {}
   }
-
+  
+  /** Call close() on the RandomAccessFile, ignoring any errors */
+  public static void safeClose(RandomAccessFile f) {
+    try {
+      f.close();
+    } catch (Exception e) {}
+  }
   /** Call release() on the LockssUrlConnection, ignoring any errors */
   public static void safeRelease(LockssUrlConnection conn) {
     try {
