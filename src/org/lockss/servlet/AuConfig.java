b75d9ad6087fdb79815c86656f40783306f55c8b
tlipkis <tlipkis>
2006-10-06 20:28:26 +0000

    *** empty log message ***

--- src/org/lockss/servlet/AuConfig.java
+++ src/org/lockss/servlet/AuConfig.java
@@ -64,7 +64,8 @@ public class AuConfig extends LockssServlet {
   static final String FOOT_CHOOSEPLUGWARN =
     "A LOCKSS " + new Link("http://www.lockss.org/lockss/Plugins", "plugin") +
     "is required to collect and preserve content. " +
-    "Manual configuration of a plugin should be used with care. " +
+    "Manual configuration of a plugin should be used with care, " +
+    "and should not be necessary for normal usage of a LOCKSS box. " +
     "It will only work with sites of the same type " +
     "for which the plugin was written.";
 
