2f34a9f3e6495c568da1fec672ee2d324e9ff6a7
thib_gc <thib_gc>
2007-02-23 19:41:34 +0000

    Cosmetic.

--- src/org/lockss/filter/pdf/ConditionalSubsequenceOperatorProcessor.java
+++ src/org/lockss/filter/pdf/ConditionalSubsequenceOperatorProcessor.java
@@ -3,8 +3,7 @@
  */
 
 /*
-
-Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+76 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
