ea4c59c60062ead95c776c76b4b3c0b693be2590
thib_gc <thib_gc>
2007-03-16 21:23:08 +0000

    Import cleanup.

--- src/org/lockss/proxy/AuditProxyManager.java
+++ src/org/lockss/proxy/AuditProxyManager.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2003 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -32,16 +32,8 @@ in this Software without prior written authorization from Stanford University.
 
 package org.lockss.proxy;
 
-import java.util.*;
-import org.lockss.app.*;
 import org.lockss.util.*;
-import org.lockss.util.urlconn.*;
 import org.lockss.config.Configuration;
-import org.lockss.daemon.*;
-import org.lockss.jetty.*;
-import org.mortbay.util.*;
-import org.mortbay.http.*;
-import org.mortbay.http.handler.*;
 
 /** Audit proxy manager, starts a proxy that serves only content from the
  * cache, useful for auditing the content.
