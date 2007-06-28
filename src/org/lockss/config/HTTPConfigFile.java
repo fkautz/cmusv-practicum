5aa7aaf89101493121a21a905f89eb02a96dee57
tlipkis <tlipkis>
2007-06-28 06:06:20 +0000

    Plug unreleased connection leak.

--- src/org/lockss/config/HTTPConfigFile.java
+++ src/org/lockss/config/HTTPConfigFile.java
@@ -126,12 +126,15 @@ public class HTTPConfigFile extends BaseConfigFile {
       break;
     case HttpURLConnection.HTTP_NOT_FOUND:
       m_loadError = resp + ": " + respMsg;
+      IOUtil.safeRelease(conn);
       throw new FileNotFoundException(m_loadError);
     case HttpURLConnection.HTTP_FORBIDDEN:
       m_loadError = findErrorMessage(resp, conn);
+      IOUtil.safeRelease(conn);
       throw new IOException(m_loadError);
     default:
       m_loadError = resp + ": " + respMsg;
+      IOUtil.safeRelease(conn);
       throw new IOException(m_loadError);
     }
 
