02821a0d6ba533eb63f0ad40191138def1d8b2b3
thib_gc <thib_gc>
2007-07-18 15:51:41 +0000

    Basic Blogger plugin.

--- plugins/src/org/lockss/plugin/blogger/BasicBloggerFilterRule.java
+++ plugins/src/org/lockss/plugin/blogger/BasicBloggerFilterRule.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -39,7 +39,7 @@ import org.lockss.filter.*;
 import org.lockss.plugin.FilterRule;
 import org.lockss.util.ListUtil;
 
-public class BloggerFilterRule implements FilterRule {
+public class BasicBloggerFilterRule implements FilterRule {
 
   public Reader createFilteredReader(Reader reader) {
     // Zap the Javascript call to _WidgetManager._SetPageActionUrl(url, cookie);
