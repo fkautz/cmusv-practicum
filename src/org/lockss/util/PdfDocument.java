b25372ddb1ee6e486ba17092e371e10739d7596c
thib_gc <thib_gc>
2007-07-31 08:40:05 +0000

    Upgraded to Commons IO 1.3.2. Improved the PDF filtering framework to handle referenced streams (Form XObjects).

--- src/org/lockss/util/PdfDocument.java
+++ src/org/lockss/util/PdfDocument.java
@@ -156,7 +156,8 @@ public class PdfDocument {
   }
 
   public String getMetadataAsString() throws IOException {
-    return getMetadata().getInputStreamAsString();
+    PDMetadata metadata = getMetadata();
+    return metadata == null ? null : metadata.getInputStreamAsString();
   }
 
   /**
@@ -313,11 +314,11 @@ public class PdfDocument {
   public void removePage(int index) throws IOException {
     getPdDocument().removePage(index);
   }
-  
+
   public void removePage(PdfPage pdfPage) throws IOException {
     removePage(pdfPage.getPdPage());
   }
-  
+
   /**
    * <p>Unsets the producer from the document information.</p>
    * @throws IOException if any processing error occurs.
@@ -326,7 +327,7 @@ public class PdfDocument {
   public void removeProducer() throws IOException {
     setProducer(null);
   }
-  
+
   /**
    * <p>Unsets the subject from the document information.</p>
    * @throws IOException if any processing error occurs.
@@ -468,7 +469,7 @@ public class PdfDocument {
   protected void parse() throws IOException {
     // Parse the document before using it
     getPdfParser().parse();
-    
+
     // Trivial decryption if encrypted without a password
     if (getPdDocument().isEncrypted()) {
       try {
