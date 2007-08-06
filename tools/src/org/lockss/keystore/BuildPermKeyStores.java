c1f8b24a4d3bfb6cd8f1a5b6fcd9e780735644c4
thib_gc <thib_gc>
2007-08-06 18:45:23 +0000

    Added package declaration.

--- tools/src/org/lockss/keystore/BuildPermKeyStores.java
+++ tools/src/org/lockss/keystore/BuildPermKeyStores.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2006 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,6 +29,8 @@ be used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from Stanford University.
 
 */
+
+package org.lockss.keystore;
 
 import java.util.*;
 import java.io.*;
