d567f4dd592a82e51f85b2d309414e1f50e3742b
smorabito <smorabito>
2007-09-07 03:05:53 +0000

    Fix for issue 2671

--- src/org/lockss/poller/v3/PollerStateBean.java
+++ src/org/lockss/poller/v3/PollerStateBean.java
@@ -459,23 +459,43 @@ public class PollerStateBean implements LockssSerializable {
     /* Getters are copy-on-read */
 
     public synchronized Set getAgreedUrls() {
-      return agreedUrls;
+      if (agreedUrls == null) {
+        return new HashSet();
+      } else {
+        return new HashSet(agreedUrls);
+      }
     }
     
     public synchronized Set getDisagreedUrls() {
-      return disagreedUrls;
+      if (disagreedUrls == null) {
+        return new HashSet();
+      } else {
+        return new HashSet(disagreedUrls);
+      }
     }
     
     public synchronized Set getTooCloseUrls() {
-      return tooCloseUrls;
+      if (tooCloseUrls == null) {
+        return new HashSet();
+      } else {
+        return new HashSet(tooCloseUrls);
+      }
     }
     
     public synchronized Set getNoQuorumUrls() {
-      return noQuorumUrls;
+      if (noQuorumUrls == null) {
+        return new HashSet();
+      } else {
+        return new HashSet(noQuorumUrls);
+      }
     }
     
     public synchronized Map getErrorUrls() {
-      return errorUrls;
+      if (errorUrls == null) {
+        return new HashMap();
+      } else {
+        return new HashMap(errorUrls);
+      }
     }
   }
 
