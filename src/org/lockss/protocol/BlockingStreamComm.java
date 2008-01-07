b5fa879a8607da548298d62b24b41f297f9b3939
dshr <dshr>
2008-01-07 13:32:32 +0000

    Issue 2713 - Refused SSL connections should log IP

--- src/org/lockss/protocol/BlockingStreamComm.java
+++ src/org/lockss/protocol/BlockingStreamComm.java
@@ -1015,7 +1015,7 @@ public class BlockingStreamComm
       java.security.cert.Certificate[] certs = session.getPeerCertificates();
       log.debug(session.getPeerHost() + " via " + session.getProtocol() + " verified");
     } catch (SSLPeerUnverifiedException ex) {
-      log.error(session.getPeerHost() + " threw " + ex);
+      log.error(s.getInetAddress() + " threw " + ex);
       try {
         s.close();
       } catch (IOException ex2) {
