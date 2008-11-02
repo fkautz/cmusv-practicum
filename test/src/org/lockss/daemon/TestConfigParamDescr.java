f71047193ef0d48602a6a4bcd5644a69c9cf174b
tlipkis <tlipkis>
2008-11-02 21:11:19 +0000

    Added TYPE_LONG.

--- test/src/org/lockss/daemon/TestConfigParamDescr.java
+++ test/src/org/lockss/daemon/TestConfigParamDescr.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2008 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -101,6 +101,13 @@ public class TestConfigParamDescr extends LockssTestCase {
     d4.setSize(12);
     d4.setType(ConfigParamDescr.TYPE_INT);
     assertEquals(12, d4.getSize());
+
+    ConfigParamDescr d5 = new ConfigParamDescr("k1");
+    d5.setType(ConfigParamDescr.TYPE_LONG);
+    assertEquals(10, d5.getSize());
+    d5.setSize(18);
+    assertEquals(18, d5.getSize());
+
   }
 
   public void testCompareTo() {
