190ef84051e51bce803165c80eac99ef0de8a276
tlipkis <tlipkis>
2009-01-07 22:59:02 +0000

    Don't log redundant platform overrides.

--- src/org/lockss/config/ConfigManager.java
+++ src/org/lockss/config/ConfigManager.java
@@ -1066,8 +1066,9 @@ public class ConfigManager implements LockssManager {
   }
 
   private void platformOverride(Configuration config, String key, String val) {
-    if (!StringUtil.equalStrings(config.get(key), val)) {
-      log.warning("Overriding param: " + key + "= " + config.get(key));
+    String oldval = config.get(key);
+    if (oldval != null && !StringUtil.equalStrings(oldval, val)) {
+      log.warning("Overriding param: " + key + "= " + oldval);
       log.warning("with platform-derived value: " + val);
     }
     config.put(key, val);
