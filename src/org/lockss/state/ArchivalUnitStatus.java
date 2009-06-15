e0de3603cc0c5e6db7b719fd69b29454c835088e
dshr <dshr>
2009-06-15 03:32:09 +0000

    Archival unit status pages only have links to DOI lists if their plugin supports ArticleIterator.

--- src/org/lockss/state/ArchivalUnitStatus.java
+++ src/org/lockss/state/ArchivalUnitStatus.java
@@ -886,12 +886,14 @@ public class ArchivalUnitStatus
       res.add(new StatusTable.SummaryInfo(null,
 					  ColumnDescriptor.TYPE_STRING,
 					  urlListLink));
-      res.add(new StatusTable.SummaryInfo(null,
-					  ColumnDescriptor.TYPE_STRING,
-					  doiListLink));
-      res.add(new StatusTable.SummaryInfo(null,
-					  ColumnDescriptor.TYPE_STRING,
-					  articleListLink));
+      if (!(au.getArticleIterator() == CollectionUtil.EMPTY_ITERATOR)) {
+        res.add(new StatusTable.SummaryInfo(null,
+					    ColumnDescriptor.TYPE_STRING,
+					    doiListLink));
+        res.add(new StatusTable.SummaryInfo(null,
+					    ColumnDescriptor.TYPE_STRING,
+					    articleListLink));
+      }
       return res;
     }
 
