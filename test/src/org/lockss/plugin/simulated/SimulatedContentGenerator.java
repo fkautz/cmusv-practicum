93008ffffef9f83c66cd0429194a741a18488f8e
dshr <dshr>
2009-08-29 23:17:55 +0000

    Elsevier metadata extractor and test for it. Progress toward Issue 3086.

--- test/src/org/lockss/plugin/simulated/SimulatedContentGenerator.java
+++ test/src/org/lockss/plugin/simulated/SimulatedContentGenerator.java
@@ -98,7 +98,10 @@ public class SimulatedContentGenerator {
     "<entry><key>file</key><value>%1</value></entry>" +
     "<entry><key>depth</key><value>%2</value></entry>" +
     "<entry><key>branch</key><value>%3</value></entry>" +
-    "</map>";
+    "</map>" +
+    "<ce:doi>%1.%2/%3</ce:doi>" +  // Elsevier DOI
+    "<XXX>%1.%2/%3</XXX>" + // Springer DOI
+    "<foo>bar</foo>";
   /**
    * Name of top directory in which the content is generated.
    */
