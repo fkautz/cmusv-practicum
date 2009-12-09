61a89d1984f8256dba287157531574532c42bebc
tlipkis <tlipkis>
2009-12-09 00:07:21 +0000

    Added a PluginManager variant that loads plugins regardless of their  minimum required daemon version, for testing (with daemon 0.0.0, etc.).

--- src/org/lockss/plugin/PluginManager.java
+++ src/org/lockss/plugin/PluginManager.java
@@ -4,7 +4,7 @@
 
 /*
 
-Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -1194,7 +1194,7 @@ public class PluginManager
     }
   }
 
-  boolean isCompatible(Plugin plug) {
+  protected boolean isCompatible(Plugin plug) {
     boolean res;
     DaemonVersion dver = getDaemonVersion();
     if (dver == null) {
