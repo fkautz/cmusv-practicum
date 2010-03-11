56a61c9f0a95a2254a63e521375b66b426f1aa6b
thib_gc <thib_gc>
2010-03-11 01:36:39 +0000

    Copyright year.

--- test/scripts/tdbconst.py
+++ test/scripts/tdbconst.py
@@ -2,7 +2,7 @@
 
 # $Id$
 #
-# Copyright (c) 2000-2009 Board of Trustees of Leland Stanford Jr. University,
+# Copyright (c) 2000-2010 Board of Trustees of Leland Stanford Jr. University,
 # all rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
