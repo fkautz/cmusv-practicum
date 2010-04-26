0d16d2210740530d9f7b2bc8d91371aebd6efedc
edwardsb1 <edwardsb1>
2010-04-26 23:26:17 +0000

    Reverting NatureArticleIteratorFactory.

--- plugins/src/org/lockss/plugin/nature/NatureArticleIteratorFactory.java
+++ plugins/src/org/lockss/plugin/nature/NatureArticleIteratorFactory.java
@@ -68,15 +68,9 @@ public class NatureArticleIteratorFactory implements ArticleIteratorFactory {
     String vol = au.getConfiguration().get(ConfigParamDescr.VOLUME_NAME.getKey());
     subTreeRoot = jid + "/journal/v" + vol;
     log.debug("createArticleIterator(" + mimeType + "," + au.toString() +
-              ") " + subTreeRoot);
-    
-    if (jid != "ndigest") {
-      pat = Pattern.compile("journal/v[^/]+/n[^/]+/full/",
+              ") " + subTreeRoot);    
+    pat = Pattern.compile("journal/v[^/]+/n[^/]+/full/",
           Pattern.CASE_INSENSITIVE);
-    } else {
-      pat = Pattern.compile("journal/v[^/]+/n[^/]+/pdf/",
-          Pattern.CASE_INSENSITIVE);
-    }
     
     return new SubTreeArticleIterator(mimeType, au, subTreeRoot, pat);
   }
