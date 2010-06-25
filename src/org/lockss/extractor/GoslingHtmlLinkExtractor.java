a649754f330f91a6ddc8e5adadc636869006c57e
tlipkis <tlipkis>
2010-06-25 07:40:58 +0000

    ReaderInputStream now does proper character encoding.

--- src/org/lockss/extractor/GoslingHtmlLinkExtractor.java
+++ src/org/lockss/extractor/GoslingHtmlLinkExtractor.java
@@ -538,8 +538,7 @@ public class GoslingHtmlLinkExtractor implements LinkExtractor {
       }
     };
 
-    // XXX This isn't as inefficient as it looks. ...
-    InputStream cssIn = new ReaderInputStream(cssReader);
+    InputStream cssIn = new ReaderInputStream(cssReader, encoding);
     try {
       LinkExtractor cssExtractor = au.getLinkExtractor("text/css");
       logger.debug2("CSS extractor: " + cssExtractor + ", " + au);
