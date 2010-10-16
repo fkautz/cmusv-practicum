de7f104dc2760b5195fda46b9a7ee119552674d2
thib_gc <thib_gc>
2010-10-16 01:49:49 +0000

    Additional filtering.

--- plugins/src/org/lockss/plugin/springer/SpringerLinkHtmlHashFilterFactory.java
+++ plugins/src/org/lockss/plugin/springer/SpringerLinkHtmlHashFilterFactory.java
@@ -35,7 +35,7 @@ package org.lockss.plugin.springer;
 import java.io.InputStream;
 
 import org.htmlparser.NodeFilter;
-import org.htmlparser.filters.OrFilter;
+import org.htmlparser.filters.*;
 import org.lockss.daemon.PluginException;
 import org.lockss.filter.html.*;
 import org.lockss.plugin.*;
@@ -48,6 +48,8 @@ public class SpringerLinkHtmlHashFilterFactory implements FilterFactory {
                                                String encoding)
       throws PluginException {
     NodeFilter[] filters = new NodeFilter[] {
+        // Contains ad-specific cookies
+        new TagNameFilter("script"),
         // Contains cross-links to other articles in other journals/volumes
         HtmlNodeFilters.tagWithAttribute("div", "id", "RelatedSection"),
         // Contains ads
@@ -58,6 +60,8 @@ public class SpringerLinkHtmlHashFilterFactory implements FilterFactory {
         HtmlNodeFilters.tagWithAttribute("div", "id", "MasterHeaderRecognition"),
         // Contains SFX links
         HtmlNodeFilters.tagWithAttributeRegex("div", "class", "linkoutView"),
+        // Has a session cookie
+        HtmlNodeFilters.tagWithAttribute("form", "id", "LoginForm"),
     };
     return new HtmlFilterInputStream(in,
                                      encoding,
