9ba1e64850205d31df77ec50454f4d15868c4eeb
tlipkis <tlipkis>
2011-01-20 08:38:16 +0000

    Renamed SimpleMetaTagMetadataExtractor -> SimpleHtmlMetaTagMetadataExtractor

--- src/org/lockss/extractor/SimpleHtmlMetaTagMetadataExtractor.java
+++ src/org/lockss/extractor/SimpleHtmlMetaTagMetadataExtractor.java
@@ -1,5 +1,5 @@
 /*
- * $Id$
+ * $Id: SimpleHtmlMetaTagMetadataExtractor.java,v 1.1 2011/01/20 08:38:16 tlipkis Exp $
  */
 
 /*
@@ -36,12 +36,12 @@ import java.io.*;
 import org.lockss.util.*;
 import org.lockss.plugin.*;
 
-public class SimpleMetaTagMetadataExtractor
+public class SimpleHtmlMetaTagMetadataExtractor
   extends SimpleFileMetadataExtractor {
 
-  static Logger log = Logger.getLogger("SimpleMetaTagMetadataExtractor");
+  static Logger log = Logger.getLogger("SimpleHtmlMetaTagMetadataExtractor");
 
-  public SimpleMetaTagMetadataExtractor() {
+  public SimpleHtmlMetaTagMetadataExtractor() {
   }
 
   public ArticleMetadata extract(CachedUrl cu) throws IOException {
