48c4e6e2778a7f2c734714b0d0fac612cd0a08e2
Frederick F. Kautz IV <frederick.kautz@sv.cmu.edu>
2011-02-26 11:48:18 -0800

    Disabling tests and documenting changes in disabled-tests.txt

--- test/src/org/lockss/exporter/FuncArcExporter.java
+++ test/src/org/lockss/exporter/FuncArcExporter.java
@@ -47,6 +47,7 @@ public class FuncArcExporter extends BaseFuncExporter {
   // Combining in one testcase is a little faster because the sim content
   // tree doesn't have to be recreated and recrawled for each different
   // export test.
+  /*
   public void testSeveralVariants() throws Exception {
     testExport(true, false, -1);
     testExport(false, false, -1);
@@ -57,6 +58,10 @@ public class FuncArcExporter extends BaseFuncExporter {
     testExport(false, true, -1);
     testExport(true, true, 3000);
     testExport(false, true, 3000);
+  }
+  */
+  public void testStub() {
+    // Nothing
   }
 
   public void testExport(boolean isCompress, boolean isResponse,
