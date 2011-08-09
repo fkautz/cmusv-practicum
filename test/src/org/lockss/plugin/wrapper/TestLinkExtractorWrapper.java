2eae082602ac5105d44a2a382d99f47427367338
tlipkis <tlipkis>
2011-08-09 04:17:30 +0000

    Generics.

--- test/src/org/lockss/plugin/wrapper/TestLinkExtractorWrapper.java
+++ test/src/org/lockss/plugin/wrapper/TestLinkExtractorWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id$
+ * $Id: TestLinkExtractorWrapper.java,v 1.2 2011/08/09 04:17:30 tlipkis Exp $
  */
 
 /*
@@ -45,8 +45,7 @@ public class TestLinkExtractorWrapper extends LockssTestCase {
 
   public void testWrap() throws PluginException, IOException {
     LinkExtractor obj = new MockLinkExtractor();
-    LinkExtractor wrapper =
-      (LinkExtractor)WrapperUtil.wrap(obj, LinkExtractor.class);
+    LinkExtractor wrapper = WrapperUtil.wrap(obj, LinkExtractor.class);
     assertTrue(wrapper instanceof LinkExtractorWrapper);
     assertTrue(WrapperUtil.unwrap(wrapper) instanceof MockLinkExtractor);
 
@@ -58,8 +57,7 @@ public class TestLinkExtractorWrapper extends LockssTestCase {
 
   public void testLinkageError() throws IOException {
     LinkExtractor obj = new MockLinkExtractor();
-    LinkExtractor wrapper =
-      (LinkExtractor)WrapperUtil.wrap(obj, LinkExtractor.class);
+    LinkExtractor wrapper = WrapperUtil.wrap(obj, LinkExtractor.class);
     assertTrue(wrapper instanceof LinkExtractorWrapper);
     assertTrue(WrapperUtil.unwrap(wrapper) instanceof MockLinkExtractor);
     Error err = new LinkageError("bar");
